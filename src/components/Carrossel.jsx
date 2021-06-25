e42f330dcb6d6ea407b4f193d5c4e46acf51d404
Gabriela Prizo <gabbprizo@gmail.com>
2021-06-25 12:55:39 -0300

    Alterando título das fotos do carrossel.

--- src/components/Carrossel.jsx
+++ src/components/Carrossel.jsx
@@ -6,17 +6,17 @@ function Carrossel(props)
 {
     const items = [
         {
-            name: "Random Name #1",
+            name: "Pokemon, temos que pegar!",
             description: "Probably the most random thing you have ever seen!",
             image: "https://4.bp.blogspot.com/-UYJjvmDemn0/WT3ov0R6cpI/AAAAAAAAvgE/frFGuJ4p4Ac7umWkdZZF5qTbnKdReWP2wCLcB/s1600/landscape-1456483171-pokemon2.jpg"
         },
         {
-            name: "Random Name #2",
+            name: "Eu escolho você!",
             description: "Hello World!",
             image: "https://assets.pokemon.com/assets//cms2-pt-br/img/watch-pokemon-tv/_tiles/broadcaster/season23-cartoon-network-169.png"
         },
         {
-            name: "Random Name #3",
+            name: "Vamos duelar!",
             description: "Olá mundo!",
             image: "https://static-wp-tor15-prd.torcedores.com/wp-content/uploads/2021/03/pokemon-go-programa-indicacoes-676x338.jpg"
         }
