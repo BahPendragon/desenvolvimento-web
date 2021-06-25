97b213c37718f6a612d0229ed49ff2b21c064311
Gabriela Prizo <gabbprizo@gmail.com>
2021-06-25 00:34:55 -0300

    Alterações do React DOM

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -73,15 +73,3 @@ export default function ContainedButtons() {
     </>
   );
 }
-
-function Sobre() {
-    return(
-    <div>
-        <h1>Sobre</h1>
-            <br />
-        <Link to="/produtos">Produtos</Link>
-    </div>)
-}
-
-
-export default Sobre;
